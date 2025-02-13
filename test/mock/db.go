da144f88fee5fb5783ad1e3a5c9e9ddd30f51731
Jemiah Westerman <jemiahw@gmail.com>
2025-02-13 12:13:36 -0800

    Create a Plugin for database connection (#65)
    This will allow custom connection logic, such as to wrap the connection for monitoring.

---
+++ test/mock/db.go
@@ -0,0 +1,18 @@
+package mock
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/square/etre/config"
+	"github.com/square/etre/db"
+)
+
+var _ db.Plugin = DBPlugin{}
+
+type DBPlugin struct {
+	ConnectFunc func(config.DatasourceConfig) (*mongo.Client, error)
+}
+
+func (d DBPlugin) Connect(cfg config.DatasourceConfig) (*mongo.Client, error) {
+	return d.ConnectFunc(cfg)
+}
